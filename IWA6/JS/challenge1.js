e18413cf1a489f48ee86dfbd821d9865787f572e
BlazeTheGr8 <adamcastle09@icloud.com>
2023-04-05 22:06:57 +0200

    uploaded code

---
+++ IWA6/JS/challenge1.js
@@ -0,0 +1,22 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid =  !isNaN(parseInt(primaryPhone))
+const secondaryValid = !isNaN(parseInt(secondaryPhone)); 
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
+
+
+// const primaryPhone = "O748105141";
+// const secondaryPhone = "0219131568";
+
+// // Only change below this line
+
+// const primaryValid = typeof number == primaryPhone;
+// const secondaryValid = typeof number == secondaryPhone;
+
+// console.log("Primary phone is valid numerical string:", primaryValid);
+// console.log("Secondary phone is valid numerical string:", secondaryValid);
