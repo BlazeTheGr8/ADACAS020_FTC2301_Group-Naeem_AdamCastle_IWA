f9016f8440452ad149d9ee59591c255dfb7c9460
BlazeTheGr8 <adamcastle09@icloud.com>
2023-04-11 14:25:08 +0200

    uploaded IWA9

--- IWA8/JS/challenge1.js
+++ IWA8/JS/challenge1.js
@@ -28,7 +28,7 @@ const leo = {
 }
 
 const sarah = {
-	names: sarahName + sarahSurname,
+	names: sarahName.trimEnd() + " " + sarahSurname,
 	age: 62,
 	accessId: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
 	balance: sarahBalance,
