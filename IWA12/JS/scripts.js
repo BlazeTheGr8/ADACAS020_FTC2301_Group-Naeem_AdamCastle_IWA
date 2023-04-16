21b320e7d20f26c43efff3e10ea093c58aafef56
BlazeTheGr8 <adamcastle09@icloud.com>
2023-04-16 20:04:26 +0200

    refined code

--- IWA12/JS/scripts.js
+++ IWA12/JS/scripts.js
@@ -29,27 +29,51 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-const staytus = document.querySelectorAll('p .status');
+const Status = document.querySelectorAll('.status');
 const reserve = document.querySelectorAll('.reserve')
 const checkout = document.querySelectorAll('.checkout')
 const checkin = document.querySelectorAll('.checkin')
 
 for (i = 0; i < checkin.length; i++) {
-    checkin[i].style.color = "";
+  checkin[i].style.color = "";
+  Status[i].style.color = STATUS_MAP[Status[i].textContent].color;
+  reserve[i].disabled = STATUS_MAP[Status[i].textContent].canReserve ? false : true;
+  checkout[i].disabled = STATUS_MAP[Status[i].textContent].canCheckout ? false : true;
+  checkin[i].disabled = STATUS_MAP[Status[i].textContent].canCheckIn ? false : true;
 }
 
-staytus[0].style.color = STATUS_MAP.overdue.color
-reserve[0].disabled = STATUS_MAP.overdue.canReserve ? false : true;
-checkout[0].disabled = STATUS_MAP.overdue.canCheckout ? false : true;
-checkin[0].disabled = STATUS_MAP.overdue.canCheckIn ? false : true;
+// for (i = 0; i < Status.length; i++) {
+//   Status[i].style.color = STATUS_MAP[Status[i].textContent].color;
 
-staytus[1].style.color = STATUS_MAP.reserved.color
-reserve[1].disabled = STATUS_MAP.reserved.canReserve ? false : true;
-checkout[1].disabled = STATUS_MAP.reserved.canCheckout ? false : true;
-checkin[1].disabled = STATUS_MAP.reserved.canCheckIn ? false : true;
+//   reserve[i].disabled = STATUS_MAP[Status[i].textContent].canReserve
+//     ? false
+//     : true;
 
-staytus[2].style.color = STATUS_MAP.shelf.color;
-reserve[2].disabled = STATUS_MAP.shelf.canReserve ? false : true;
-checkout[2].disabled = STATUS_MAP.shelf.canCheckout ? false : true;
-checkin[2].disabled = STATUS_MAP.shelf.canCheckIn ? false : true;
+//       checkout[i].disabled = STATUS_MAP[Status[i].textContent].canCheckout ? false : true;
+
+//       checkin[i].disabled = STATUS_MAP[Status[i].textContent].canCheckIn ? false : true;
+// }
+
+// for (i = 0; i < reserve.length; i++) {
+//     reserve[i].disabled = STATUS_MAP[Status[i].textContent].canReserve ? false : true;
+
+//     checkout[i].disabled = STATUS_MAP[Status[i].textContent].canCheckout ? false : true;
+
+//     checkin[i].disabled = STATUS_MAP[Status[i].textContent].canCheckIn ? false : true;
+// }
+
+// Status[0].style.color = STATUS_MAP[stattus[0].textContent].color;
+// reserve[0].disabled = STATUS_MAP.overdue.canReserve ? false : true;
+// checkout[0].disabled = STATUS_MAP.overdue.canCheckout ? false : true;
+// checkin[0].disabled = STATUS_MAP.overdue.canCheckIn ? false : true;
+
+// Status[1].style.color = STATUS_MAP.reserved.color
+// reserve[1].disabled = STATUS_MAP.reserved.canReserve ? false : true;
+// checkout[1].disabled = STATUS_MAP.reserved.canCheckout ? false : true;
+// checkin[1].disabled = STATUS_MAP.reserved.canCheckIn ? false : true;
+
+// Status[2].style.color = STATUS_MAP.shelf.color;
+// reserve[2].disabled = STATUS_MAP.shelf.canReserve ? false : true;
+// checkout[2].disabled = STATUS_MAP.shelf.canCheckout ? false : true;
+// checkin[2].disabled = STATUS_MAP.shelf.canCheckIn ? false : true;
 
