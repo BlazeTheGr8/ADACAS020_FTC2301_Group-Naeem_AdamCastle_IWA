ff7be2cb98133ed2059b9410eb59f5f701dc9263
BlazeTheGr8 <adamcastle09@icloud.com>
2023-04-20 13:39:24 +0200

    uploaded IWA15

--- IWA12/JS/scripts.js
+++ IWA12/JS/scripts.js
@@ -34,46 +34,11 @@ const reserve = document.getElementsByClassName('reserve')
 const checkout = document.getElementsByClassName('checkout')
 const checkin = document.getElementsByClassName('checkin')
 
-for (i = 0; i < checkin.length; i++) {
+for (let i = 0; i < checkin.length; i++) {
   checkin[i].style.color = "";
   Status[i].style.color = STATUS_MAP[Status[i].textContent].color;
   reserve[i].disabled = STATUS_MAP[Status[i].textContent].canReserve ? false : true;
   checkout[i].disabled = STATUS_MAP[Status[i].textContent].canCheckout ? false : true;
   checkin[i].disabled = STATUS_MAP[Status[i].textContent].canCheckIn ? false : true;
 }
-
-// for (i = 0; i < Status.length; i++) {
-//   Status[i].style.color = STATUS_MAP[Status[i].textContent].color;
-
-//   reserve[i].disabled = STATUS_MAP[Status[i].textContent].canReserve
-//     ? false
-//     : true;
-
-//       checkout[i].disabled = STATUS_MAP[Status[i].textContent].canCheckout ? false : true;
-
-//       checkin[i].disabled = STATUS_MAP[Status[i].textContent].canCheckIn ? false : true;
-// }
-
-// for (i = 0; i < reserve.length; i++) {
-//     reserve[i].disabled = STATUS_MAP[Status[i].textContent].canReserve ? false : true;
-
-//     checkout[i].disabled = STATUS_MAP[Status[i].textContent].canCheckout ? false : true;
-
-//     checkin[i].disabled = STATUS_MAP[Status[i].textContent].canCheckIn ? false : true;
-// }
-
-// Status[0].style.color = STATUS_MAP[stattus[0].textContent].color;
-// reserve[0].disabled = STATUS_MAP.overdue.canReserve ? false : true;
-// checkout[0].disabled = STATUS_MAP.overdue.canCheckout ? false : true;
-// checkin[0].disabled = STATUS_MAP.overdue.canCheckIn ? false : true;
-
-// Status[1].style.color = STATUS_MAP.reserved.color
-// reserve[1].disabled = STATUS_MAP.reserved.canReserve ? false : true;
-// checkout[1].disabled = STATUS_MAP.reserved.canCheckout ? false : true;
-// checkin[1].disabled = STATUS_MAP.reserved.canCheckIn ? false : true;
-
-// Status[2].style.color = STATUS_MAP.shelf.color;
-// reserve[2].disabled = STATUS_MAP.shelf.canReserve ? false : true;
-// checkout[2].disabled = STATUS_MAP.shelf.canCheckout ? false : true;
-// checkin[2].disabled = STATUS_MAP.shelf.canCheckIn ? false : true;
 
